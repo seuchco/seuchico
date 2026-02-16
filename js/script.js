315d0f39ac588540679a512df72cb46b33bc399d
Claudio Santos <ss.filho@outlook.com>
2026-02-16 11:46:17 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let lastScroll = 0;
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll > lastScroll && currentScroll > 100) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+
+  lastScroll = currentScroll;
+});
+
